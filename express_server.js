897a3896e8326b7bf695b85a22b3454ee6e5ac7f
kethnal <kaseyvaldez@hotmail.com>
2022-10-04 12:46:19 -0700

    debug templateVars to show longURL on single url pages

--- express_server.js
+++ express_server.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
-    longURL: req.params.longURL
+    longURL: urlDatabase[req.params.id]
   };
   res.render("urls_show", templateVars);
 });
