c7e5407276dd910ecffd55c7f88207d53f87f915
kethnal <kaseyvaldez@hotmail.com>
2022-10-04 12:30:26 -0700

    add page that displays single urls

--- express_server.js
+++ express_server.js
@@ -13,6 +13,14 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: req.params.longURL
+  };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
