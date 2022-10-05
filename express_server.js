04fc51b1b47c5421ac2d6e1d186af68d33376fe5
kethnal <kaseyvaldez@hotmail.com>
2022-10-05 12:14:43 -0700

    add login in header and store username cookies

--- express_server.js
+++ express_server.js
@@ -1,4 +1,6 @@
+const cookieParser = require('cookie-parser');
 const express = require("express");
+ 
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -13,6 +15,7 @@ const generateRandomString = function() {
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -35,6 +38,26 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body);
+  console.log(req.body); // DEBUGGING
+  res.redirect(`/urls`);
+});
+
 app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = `http://www.${req.body.longURL}`;
   console.log(urlDatabase); // DEBUGGING
@@ -52,24 +75,6 @@ app.post("/urls", (req, res) => {
   urlDatabase[id] = `http://www.${req.body.longURL}`;
   console.log(urlDatabase); // DEBUGGING
   res.redirect(`/urls/${id}`);
-});
-
-app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
-});
-
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
