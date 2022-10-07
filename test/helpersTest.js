5c56316387a268e96fa42961fe35a02de53ce84a
kethnal <kaseyvaldez@hotmail.com>
2022-10-06 17:30:58 -0700

    make tests for other rest of helper functions

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,7 +1,9 @@
 const { assert } = require('chai');
 
-const { findUserByEmail } = require('../helpers.js');
+const { generateRandomString, urlsForUser, findUserByEmail } = require('../helpers.js');
 
+
+// EXAMPLE DATABASES
 const testUsers = {
   "userRandomID": {
     id: "userRandomID",
@@ -14,6 +16,46 @@ const testUsers = {
     password: "dishwasher-funk"
   }
 };
+
+const testUrlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "userRandomID",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+
+// TESTS
+describe('generateRandomString', function() {
+  it('should true if both strings are different', function() {
+    const string1 = generateRandomString();
+    const string2 = generateRandomString();
+    assert.notStrictEqual(string1, string2);
+  });
+});
+
+describe('urlsForUser', function() {
+  it('should return object with urls made by user only', function() {
+    const urls = urlsForUser("userRandomID", testUrlDatabase);
+    const newObject = {
+      b6UTxQ: {
+        longURL: "https://www.tsn.ca",
+        userID: "userRandomID"
+      }
+    };
+    assert.deepEqual(urls, newObject);
+  });
+
+  it('should return empty object if user owns no urls', function() {
+    const urls = urlsForUser("user2RandomID", testUrlDatabase);
+    const newObject = {};
+    assert.deepEqual(urls, newObject);
+  });
+});
 
 describe('findUserByEmail', function() {
   it('should return a user with valid email', function() {
